93fddf939696b0a6fcf6b1758ac3cb616dccce03
expelthegrace <32312668+expelthegrace@users.noreply.github.com>
2017-11-18 19:33:35 +0100

    omegalul

--- Assets/Scripts/grass.cs
+++ Assets/Scripts/grass.cs
@@ -5,28 +5,31 @@ using UnityEngine;
 public class grass : MonoBehaviour {
 
     public GameObject arbre;
-    public GameObject player;
     public float step;
     private float index;
 
     // Use this for initialization
     void Start()
     {
-        step = player.GetComponent<PlayerMove>().step;
-        index = GetComponent<Collider>().bounds.min.x; // començo per l'esquerra
+        step = GameObject.Find("player").GetComponent<PlayerMove>().step;
+        index = GetComponent<Collider>().bounds.min.x + step / 2; // començo per l'esquerra
+        Vector3 inipos;
+        Debug.Log(index);
+
+        for (int i = 0; i < GetComponent<Collider>().bounds.size.x / step; ++i)
+        {
+            if (Random.Range(0,100) < 35)
+            {
+
+                inipos = new Vector3(index + i * step, GetComponent<Collider>().bounds.max.y, transform.position.z);
+                Instantiate(arbre, inipos, Quaternion.Euler(0, 0, 0), GameObject.Find("Arbres").transform);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 inipos;
-        for (int i = 0; i < GetComponent<Collider>().bounds.size.z / step; ++i)
-        {
-            inipos = new Vector3(index + i * step / 2, GetComponent<Collider>().bounds.max.y, transform.position.z + step / 2);
-            Instantiate(arbre, inipos, Quaternion.Euler(0, 0, 0), GameObject.Find("Arbres").transform);
-        }
-
-        // Vector3 inipos = new Vector3(GetComponent<Collider>().bounds.max.x, GetComponent<Collider>().bounds.max.y, transform.position.z);
-        //  GameObject car = Instantiate(arbre, inipos, Quaternion.Euler(0, 0, 0), GameObject.Find("Cotxes").transform);
+       
     }
 }
