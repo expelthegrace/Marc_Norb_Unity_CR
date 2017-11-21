c1fe5798e531b7525ac9951f4b9c01f6a8a5ebb5
expelthegrace <32312668+expelthegrace@users.noreply.github.com>
2017-11-21 16:51:44 +0100

    gggg

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -16,6 +16,7 @@ public class PlayerMove : MonoBehaviour {
     public bool saltant;
     public bool landed;
     private bool intronc;
+    private GameObject checkFront;
 
     private Transform tronc;
     public float troncOffset;
@@ -36,6 +37,9 @@ public class PlayerMove : MonoBehaviour {
     private RaycastHit hit;
 
     private bool enMoviment;
+
+    public float dstX;
+    public int chunkAct;
     // Use this for initialization
     void Start() {
         speed = 13;
@@ -47,6 +51,7 @@ public class PlayerMove : MonoBehaviour {
         saltant = false;
         landed = true;
         intronc = false;
+        checkFront = GameObject.Find("checkFront");
     }
 
     void reset()
@@ -66,7 +71,7 @@ public class PlayerMove : MonoBehaviour {
         if (Input.GetKey(KeyCode.UpArrow) && !enMoviment)
         {   // si no hi ha una paret
 
-            if (!Physics.Raycast(transform.position + new Vector3(0, 3, 0), Vector3.forward, out hit, step + step / 3) || hit.collider.gameObject.tag != "wall")
+            if (!Physics.Raycast(transform.position + new Vector3(0, 3, 0), Vector3.forward, out hit, step + step / 3) || hit.collider.gameObject.tag != "wall" && !checkFront.GetComponent<checkFront>().wall)
             {
                 if (direccio != frontDir)
                 {
@@ -74,13 +79,20 @@ public class PlayerMove : MonoBehaviour {
                     direccio = frontDir;
                 }
                 srcPosition = transform.position;
-                dstPosition = transform.position + direccio * step;
+                dstX = ((int)(transform.position.x + direccio.x * step) / (int)step) * step;
+                //transform.position.x + direccio.x * step
+                dstPosition = new Vector3(dstX, transform.position.y + direccio.y * step, transform.position.z + direccio.z * step);
                 tStep = 0;
                 yPos = transform.position.y;
                 saltant = true;
                 enMoviment = true;
             }
-            else Debug.Log("Colisio frontal");
+            else
+            {
+                Debug.Log("Colisio frontal");
+                transform.rotation = Quaternion.LookRotation(frontDir);
+                direccio = frontDir;
+            }
         }
 
         else if (Input.GetKey(KeyCode.RightArrow) && !enMoviment)
@@ -99,7 +111,12 @@ public class PlayerMove : MonoBehaviour {
                 saltant = true;
                 enMoviment = true;
             }
-            else Debug.Log("Colisio dreta");
+            else
+            {
+                Debug.Log("Colisio dreta");
+                transform.rotation = Quaternion.LookRotation(rightDir);
+                direccio = rightDir;
+            }
         }
 
         else if (Input.GetKey(KeyCode.DownArrow) && !enMoviment)
@@ -119,7 +136,12 @@ public class PlayerMove : MonoBehaviour {
                 enMoviment = true;
 
             }
-            else Debug.Log("Colisio atras");
+            else
+            {
+                Debug.Log("Colisio trasera");
+                transform.rotation = Quaternion.LookRotation(downDir);
+                direccio = downDir;
+            }
 
         }
 
@@ -139,7 +161,12 @@ public class PlayerMove : MonoBehaviour {
                 saltant = true;
                 enMoviment = true;
             }
-            else Debug.Log("Colisio esquerre");
+            else
+            {
+                Debug.Log("Colisio trasera");
+                transform.rotation = Quaternion.LookRotation(leftDir);
+                direccio = leftDir;
+            }
 
         }
     }
@@ -166,6 +193,8 @@ public class PlayerMove : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        chunkAct = (int)transform.position.x / (int)step;
+       
         // orientar el player i comprovar obstacles
         orientar();
         // moure/saltar player step by step
@@ -191,7 +220,7 @@ public class PlayerMove : MonoBehaviour {
                 intronc = true;
                 tronc = hit.collider.transform;
                 troncOffset = transform.position.x - tronc.position.x;
-                Debug.Log("tronc");
+                //Debug.Log("tronc");
             }
             else if (hit.collider.gameObject.tag == "mortal") reset();
         }
@@ -206,4 +235,5 @@ public class PlayerMove : MonoBehaviour {
     }
 }
 
-/* el dstPosition no pot ser trivial al sortir dels troncs, s'ha de calcular el chunk actual i enviarlo alla  */
+/* el surt de colisio del ckeck no surt quan toca, surt abans
+ *   */
---
+++ Assets/Scripts/checkFront.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkFront : MonoBehaviour {
+
+    public bool wall;
+
+	// Use this for initialization
+	void Start () {
+        wall = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("colisio checkFront");
+        if (col.GetComponent<Collider>().tag == "wall") wall = true;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        Debug.Log("surt checkFront");
+        wall = false;
+    }
+}
