c383eabfa1816c36c82a85ccb93fbb79cb38f47a
expelthegrace <32312668+expelthegrace@users.noreply.github.com>
2017-12-28 17:04:53 +0100

    esquelet bootleg
    animacio de salt molt llarga i no se perque la puta base esta a la cintura

---
+++ Assets/Animations/skellyAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class skellyAnim : MonoBehaviour {
+    Animator anim;
+    int jump = Animator.StringToHash("jump");
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            anim.SetTrigger(jump);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            anim.SetTrigger(jump);
+        }
+
+    }
+}
